4db008ae25a3db14dc9d0b6af7cf03fc2a314fea
Philip McNamara <phil@tapmap.com>
2013-02-23 20:26:35 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pinterest::Application.config.secret_token = '59de1b1db9fe627c1c1d92956bc095c9e51f9be1708b7fba848584a21cd4809271e1682b1c227fae7eb74b69d0ff33fda3c3f38d848431360abec470404aad19'
