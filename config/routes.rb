2b12c7060ec4d0388fa8f33c41035634d341c78a
Philip McNamara <phil@tapmap.com>
2013-02-24 16:38:41 -0800

    Added Bootstrap and new colours, chnages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Pinterest::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#Home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
