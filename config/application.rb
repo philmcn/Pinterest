d0ec98bfd1e947a49fc692396c4dcc00562c6293
HTP <htp@htp-desktop.(none)>
2013-11-14 13:51:31 +0530

    changes for precompiling

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module Pinterest
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
     #required for Devise on Heroku
   end
 end
