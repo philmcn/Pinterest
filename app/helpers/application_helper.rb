4454e2fcf723886c85d7cefc8d475cb191f4fb41
HTP <htp@htp-desktop.(none)>
2013-11-15 12:37:27 +0530

    changes for youtube video

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
 	    youtube_id = $5
 	  end
 
-	  %Q{<iframe title="YouTube video player" width="500" height="300" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+	  %Q{<iframe title="YouTube video player" width="200" height="200" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
 	end
 
 end
