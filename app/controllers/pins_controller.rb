e12df7a163243aae6a0766e56e17f4966725f453
HTP <htp@htp-desktop.(none)>
2013-11-22 11:43:19 +0530

    change facebook api keys

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,7 +4,7 @@ class PinsController < ApplicationController
   # GET /pins.json
   def index
  
-    @pins = Pin.order("created_at desc").page(params[:page]).per_page(25)
+    @pins = Pin.order("created_at desc").page(params[:page]).per_page(10)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @pins }
