3c18ee3af7016a993745e02d92a1b9dab44c325d
HTP <htp@htp-desktop.(none)>
2013-12-16 17:56:05 +0530

    create feed.html and pins_feed.html

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -9,6 +9,7 @@ class PinsController < ApplicationController
 
      respond_to do |format|
       format.html
+      format.mobile
       format.rss { render :layout => false }
       format.xml
       format.json { render json: @pins }
