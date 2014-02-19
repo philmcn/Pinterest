887118e3877823162be76bf17391b44b37bf9279
lechicuong <lechicuong196@gmail.com>
2014-02-20 03:57:35 +0700

    Implement: Categories management

--- db/migrate/20131207115754_add_brand_to_pins.rb
+++ db/migrate/20131207115754_add_brand_to_pins.rb
@@ -1,6 +1,6 @@
 class AddBrandToPins < ActiveRecord::Migration
   def change
-    add_column :pins, :brand, :string
-    add_index :pins, :brand
+    #add_column :pins, :brand, :string
+    #add_index :pins, :brand
   end
 end
